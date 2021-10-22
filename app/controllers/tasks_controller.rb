4747a1102fa05b8965e45c16fa7ae5b9d112fe3c
Rainey Lopes <rainey.lopes@gmail.com>
2021-10-22 08:45:41 -0300

    Rails Active Records Basic

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,45 @@
 class TasksController < ApplicationController
+  before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    # poderia passar no "path" apenas a variável @task
+    redirect_to task_path(@task.id)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_params)
+    # poderia passar no "path" apenas a variável @task
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_restaurant
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # List all tasks
+  get 'tasks', to: 'tasks#index'
+
+  # show create form (why new needs becoming before id?)
+  get 'tasks/new', to: 'tasks#new'
+
+  # List one task
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+
+  # create the task
+  post 'tasks', to: 'tasks#create'
+
+  # show edit form
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+
+  # update the task
+  patch 'tasks/:id', to: 'tasks#update'
+
+  # delete the task
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
